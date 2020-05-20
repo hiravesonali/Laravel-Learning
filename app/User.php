ee0d82c2da32990e7e062f4b043fb96f135a8f77
Sonali Hirave <hiravesonali@gmail.com>
2020-05-20 16:24:02 +0200

    basic eloquent relationship

--- app/User.php
+++ app/User.php
@@ -36,4 +36,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function article()
+    {
+        return $this->hasMany(Article::class);  // select * from Article where user_id = 1
+
+    }
+
+    public function project()
+    {
+        return $this->hasMany(Project::class); // select * from Project where user_id = 1
+    }
+
 }
+// relations in eloquent model
+// hasMany
+// hasOne
+// belongsTo
+// belongsToMany
